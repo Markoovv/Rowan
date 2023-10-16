c6a0e681d4b4e2125995edae9ca28583fcd0bbfc
VladZodchey <igor.nerdov@gmail.com>
2023-10-16 16:33:11 +0300

    Added polls

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Rowan bot by VladZodchey
+
 import discord
 import configparser
 from numexpr import evaluate as evalu
@@ -9,7 +11,8 @@ client = discord.Client(intents=discord.Intents.all(), options=8)
 
 token = config['Prefs']['token']
 version = config['Prefs']['version']
-cursewords = config['Prefs']['cursewords']
+cursewords = config['Prefs']['cursewords'].split(',')
+votes = config['Prefs']['votes'].split(',')
 
 @client.event
 async def on_ready():
@@ -18,11 +21,11 @@ async def on_ready():
 async def on_message(msg):
     if msg.author == client.user:
         return
-    if any(word in msg.content.lower() for word in cursewords.split(',')):
+    if any(word in msg.content.lower() for word in cursewords):
         await msg.delete()
     match msg.content:
         case '&help':
-            await msg.channel.send("Here's a list of implemented commands:\n&help - Displays this msg\n&ask <q> - gives you yes/no answer for q question\n&dice <n> - rolls a dice with n sides\n&eval <p> - evaluates p problem\n&blable <m> - sends m msg\n&info - displays my portfolio carrd.co.")
+            await msg.channel.send("Here's a list of implemented commands:\n&help - Displays this msg\n&ask <q> - gives you yes/no answer for q question\n&dice <n> - rolls a dice with n sides\n&eval <p> - evaluates p problem (Disabled)\n&blable <m> - sends m msg\n&info - displays my portfolio carrd.co.")
         case s if s.startswith('&blable '):
             await msg.channel.send(msg.content[7::])
         case s if s.startswith('&ask '):
@@ -43,7 +46,17 @@ async def on_message(msg):
                 await msg.channel.send(evalu(msg.content[6::]))
             except:
                 await msg.channel.send('Failed to solve problem')
-            
+        case s if s.startswith('&poll'):
+            if msg.content[6:11] == 'yesno':
+                await msg.add_reaction('\U00002714')
+                await msg.add_reaction('\U0000274C')
+            else:
+                try:
+                    for a in range(int(msg.content[6:7])):
+                        await msg.add_reaction(votes[a])
+                except:
+                    await msg.channel.send('Failed to register poll!')
+                
         case '&info':
             await msg.channel.send(f'Rowan bot ver {version}. Author carrd: https://vladzodchey.carrd.co/')
 
