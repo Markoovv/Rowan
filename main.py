61e5559c8e1301365a2232fd9d8cda584de618e8
VladZodchey <igor.nerdov@gmail.com>
2023-10-17 22:50:04 +0300

    Added number guesser

--- main.py
+++ main.py
@@ -65,6 +65,27 @@ async def purge(ctx, arg:int):
         await ctx.send('Failed to purge the chat!')
 @bot.command()
 async def rhelp(ctx):
-    await ctx.send(f"Rowan bot's prefix is {bot.command_prefix}. List of commands:\n- rhelp - displays this message.\n- info - shows bot info.\n- blable <m> - copies <m> message.\n- ask <q>- gives you yes/no answer to <q> question.\n- dice <a> - rolls a dice with <a> sides.\n- poll <a> <p> - adds <a> digit reactions to your message (max 9). enter yesno to make it a yes/no poll.\n- eval <p> - will try to solve <p> problem.\n- purge <a> - purges <a> amount of messages in chat. Requires manage messages permission!")
+    await ctx.send(f"Rowan bot's prefix is {bot.command_prefix}. List of commands:\n- rhelp - displays this message.\n- info - shows bot info.\n- blable <m> - copies <m> message.\n- ask <q>- gives you yes/no answer to <q> question.\n- dice <a> - rolls a dice with <a> sides.\n- poll <a> <p> - adds <a> digit reactions to your message (max 9). enter yesno to make it a yes/no poll.\n- eval <p> - will try to solve <p> problem.\n- purge <a> - purges <a> amount of messages in chat. Requires manage messages permission!\n- guess - starts a number guesser game.")
+@bot.command()
+async def guess(ctx):
+    num = randint(1,15)
+    await ctx.send(f'I am thinking of a number from 1 to 20. You have 4 tries and 15 seconds.')
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit()
+
+    for i in range(4):
+        try:
+            msg = await bot.wait_for('message', check=check, timeout=20.0)
+        except asyncio.TimeoutError:
+            return await ctx.send(f"Time's up! My number was {num}.")
+        guess = int(msg.content)
+        if guess == num:
+            await ctx.send(f'You won! my number was indeed {guess}!')
+            return
+        elif guess < num:
+            await ctx.send('My number is higher!')
+        else:
+            await ctx.send('My number is lower!')
+    await ctx.send(f"You ran out of tries. My number was {num}")
 
 bot.run(token)
