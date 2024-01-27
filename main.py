73561390854a1224a718bd5332c4eace12a3cadf
Всеволод <vsev.zhdanov@gmail.com>
2024-01-27 15:55:21 +0300

    Сделал configure наполовину + mute kick ban + почистил декораторы + сделал хэндлер ошибок

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Rowan bot by VladZodchey
 
-import discord, json, os, sqlite3, typing
+import discord, json, os, sqlite3, typing, asyncio, re
+from datetime import datetime, timedelta
 from discord.ext import commands
 from random import choice, randint
 from sympy import solve, sympify
@@ -22,7 +23,6 @@ for path in os.listdir("languages"):
         languages[os.path.splitext(os.path.basename(path))[0]] = json.load(open("languages/" + path, mode="r", encoding="utf"))
 
 def prefix(bot, ctx):
-    
     try:
         c.execute("SELECT prefix FROM guilds WHERE gid = ?", (ctx.guild.id,))
         return c.fetchone()[0]
@@ -51,12 +51,46 @@ def lang(ctx):
             return languages[c.fetchone()[0]]
         except:
             return languages["en"]
-
-'''@bot.event
+def swearcheck(ctx):
+    c.execute("SELECT swear FROM guilds WHERE gid = ?", (ctx.guild.id,))
+    swears = c.fetchone()
+    if swears:
+        swearlist = swears[0].split(",")
+        if any(swear in ctx.content.lower() for swear in swearlist):
+            return True
+        else:
+            return False
+    else:
+        return False
+def capscheck(ctx):
+    c.execute("SELECT caps FROM guilds WHERE gid = ?", (ctx.guild.id,))
+    response = c.fetchone()
+    if response:
+        threshold = response[0]
+        if threshold != 0:
+            alph = list(filter(str.isalpha, ctx.content))
+            if sum(map(str.isupper, alph)) / len(alph) >= threshold * 0.01:
+                return True
+            return False
+        else: return False
+    else: return False
+def linkcheck(ctx):
+    return False
+    #c.execute("SELECT ")
+@bot.event
 async def on_message(ctx):
     if ctx.author == bot.user:
         return
-    #await ctx.reply(is_direct(ctx))'''
+    if is_direct(ctx):
+        await bot.process_commands(ctx)
+    else:
+        if swearcheck(ctx) or capscheck(ctx) or linkcheck(ctx):
+            try:
+                await ctx.delete()
+            except:
+                await bot.process_commands(ctx)
+        else:
+            await bot.process_commands(ctx)
     
 
 @bot.event
@@ -65,44 +99,12 @@ async def on_member_join(ctx):
     welcome = c.fetchone()
     if welcome:
         await ctx.send(welcome[0].format(ctx.guild.name, ctx.guild.id, ctx.mention))
-@bot.event
-async def on_guild_join(ctx):
-    c.execute("SELECT * FROM guilds WHERE gid = ?", (ctx.id,))
-    if c.fetchone():
-        c.execute('''UPDATE guilds
-SET oid = ?,
-auid = NULL,
-arid = NULL,
-premium = 0,
-language = 'en',
-updates = NULL,
-caps = 0,
-url = 0,
-swear = NULL,
-enabled = 1,
-guesstime = 20,
-guessrange = 20,
-guesstries = 4,
-mathtime = 20,
-mathrange = 100,
-mathops = '+-',
-ecid = NULL,
-erid = NULL,
-count = 0,
-welcome = NULL,
-prefix = &,
-voteid = NULL,
-votemoji = 0,
-chancemoji = 0
-WHERE gid = ?''', (ctx.id))
-    else:
-        c.execute('''INSERT INTO guilds
-(gid, oid, auid, arid, premium, language, updates, caps, url, swear, enabled, guesstime, guessrange, guesstries, mathtime, mathrange, mathops, ecid, erid, count, welcome, prefix, voteid, votemoji, chancemoji)
-VALUES (?, ?, NULL, NULL, 0 ?, NULL, 0, 0, NULL, 1, 20, 20, 4, 20, 100, '+-', NULL, NULL, 0, NULL, '&', NULL, NULL, 0)''', (ctx.id, ctx.owner.id, "en"))
+
 @bot.command()
 async def foo(ctx):
     await ctx.reply(lang(ctx)["phrases"]["hello"])
-
+@commands.has_guild_permissions(manage_guild=True)
+@commands.guild_only()
 @bot.command()
 async def prefix(ctx, arg):
     try:
@@ -113,6 +115,8 @@ async def prefix(ctx, arg):
         await ctx.reply(lang(ctx)["phrases"]["prefix_success"].format(arg))
     except:
         await ctx.reply(lang(ctx)["phrases"]["prefix_fail"])
+@commands.has_guild_permissions(manage_guild=True)
+@commands.guild_only()
 @bot.command()
 async def language(ctx, arg):
     if is_direct(ctx):
@@ -126,18 +130,16 @@ async def language(ctx, arg):
             await ctx.send(lang(ctx)["phrases"]["lang_fail"])
 @bot.command()
 async def blable(ctx, * , arg):
-    # if not swearcheck(ctx):
+    #if not swearcheck(ctx.message) and not capscheck(ctx.message) and not linkcheck(ctx.message):
     await ctx.send(arg)
 @bot.command()
 async def zen(ctx):
     await ctx.send(lang(ctx)["zen"])
 @commands.has_permissions(manage_messages=True)
+@commands.guild_only()
 @bot.command()
-async def purge(ctx, arg:int):
-    try:
-        await ctx.channel.purge(limit=arg)
-    except:
-        await ctx.send(lang(ctx)["phrases"]["purge_fail"])
+async def purge(ctx, arg:int = 5):
+    await ctx.channel.purge(limit=arg)
 @bot.command()
 async def help(ctx, arg:typing.Optional[str] = "all"):
     if arg in lang(ctx)["help"]:
@@ -165,6 +167,8 @@ async def info(ctx):
         count = c.fetchone()
         if count:
             await ctx.send(lang(ctx)["phrases"]["info"].format(version, count[0]))
+        else:
+            await ctx.send(lang(ctx)["phrases"]["info_fail"])
 @bot.command()
 async def eval(ctx, *, arg):
     arg = arg.replace("^", "**")
@@ -184,6 +188,7 @@ async def eval(ctx, *, arg):
                 await ctx.send(lang(ctx)["phrases"]["eval_zero"])
         except:
             await ctx.send(lang(ctx)["phrases"]["eval_fail"])
+@commands.cooldown(1, 30, commands.BucketType.user)
 @bot.command()
 async def guess(ctx):
     if is_direct(ctx): time = 10; rang = 20; tris = 4
@@ -200,23 +205,24 @@ async def guess(ctx):
         else: time = 10; rang = 20; tris = 4
     num = randint(1, rang)
     await ctx.send(lang(ctx)["phrases"]["guess_start"].format(rang, time, tris))
-    def check(msg): 
-        return msg.author == ctx.author and msg.channel == ctx.channel and msg.content.lstrip('-').isdigit()
+    def check(m): 
+        return m.author == ctx.author and m.channel == ctx.channel and m.content.lstrip('-').isdigit()
     for i in range(tris):
         try:
             msg = await bot.wait_for('message', check=check, timeout=time)
-        except:
-            return await ctx.send(lang(ctx)["phrases"]["guess_fail_time"].format(num))
-        guess = int(msg.content)
-        if guess == num:
-            await ctx.send(lang(ctx)["phrases"]["guess_win"].format(num))
-            return
-        else:
-            if guess > num:
-                await ctx.send(lang(ctx)["phrases"]["guess_smaller"])
+            guess = int(msg.content)
+            if guess == num:
+                await ctx.send(lang(ctx)["phrases"]["guess_win"].format(num))
+                return
             else:
-                await ctx.send(lang(ctx)["phrases"]["guess_bigger"])
+                if guess > num:
+                    await ctx.send(lang(ctx)["phrases"]["guess_smaller"])
+                else:
+                    await ctx.send(lang(ctx)["phrases"]["guess_bigger"])
+        except asyncio.TimeoutError:
+            return await ctx.send(lang(ctx)["phrases"]["guess_fail_time"].format(num))
     await ctx.send(lang(ctx)["phrases"]["guess_fail_tries"].format(num))
+@commands.cooldown(1, 10, commands.BucketType.user)
 @bot.command()
 async def math(ctx):
     if is_direct(ctx): 
@@ -241,19 +247,228 @@ async def math(ctx):
     expr = str(randint(rang * -1, rang)) + choice(oper) + str(neg(randint(rang * -1, rang))) #str(randint(rang * -1, rang))
     num = round(float(evaluate(expr)), 2)
     await ctx.send(lang(ctx)["phrases"]["math_start"].format(expr, time))
-    def check(msg): 
+    def check(m): 
         try:
-            float(msg.content)
-            return msg.author == ctx.author and msg.channel == ctx.channel
+            float(m.content)
+            return m.author == ctx.author and m.channel == ctx.channel
         except:
             return False
     try:
         msg = await bot.wait_for('message', check=check, timeout=time)
-    except:
+        guess = float(msg.content)
+        if guess == num:
+            await ctx.send(lang(ctx)["phrases"]["math_win"].format(num))
+        else:
+            await ctx.send(lang(ctx)["phrases"]["math_fail_wrong"].format(num))
+    except asyncio.TimeoutError:
         await ctx.send(lang(ctx)["phrases"]["math_fail_time"].format(num))
-    guess = float(msg.content)
-    if guess == num:
-        await ctx.send(lang(ctx)["phrases"]["math_win"].format(num))
+    
+@commands.has_permissions(manage_guild=True)
+@commands.guild_only()
+@bot.command()
+async def configure(ctx, comm=None, value1=None, value2=None):
+    print(comm, value1, value2)
+    match comm:
+        case "caps":
+            try:
+                value = int(value1)
+                if value <= 100 and value >= 0:
+                    c.execute("UPDATE guilds SET caps = ? WHERE gid = ?", (value, ctx.guild.id))
+                    base.commit()
+                else:
+                    raise ValueError
+            except sqlite3.DatabaseError:
+                await ctx.send(lang(ctx)["phrases"]["error_db"])
+            except ValueError:
+                await ctx.send(lang(ctx)["phrases"]["configure_fail_int"])
+        case "url":
+            try:
+                value = int(value1)
+                if value == 0 or value == 1:
+                    c.execute("UPDATE guilds SET url = ? WHERE gid = ?", (value, ctx.guild.id,))
+                else:
+                    raise ValueError
+            except sqlite3.DatabaseError:
+                await ctx.send(lang(ctx)["phrases"]["error_db"])
+            except ValueError:
+                await ctx.send(lang(ctx)["phrases"]["configure_fail_other"])
+        case "math":
+            match value1:
+                case "range":
+                    try:
+                        value = int(value2)
+                        if value <= 1000 and value >= 5:
+                            c.execute("UPDATE guilds SET mathrange = ? WHERE gid = ?", (value, ctx.guild.id,))
+                            base.commit()
+                            await ctx.send(lang(ctx)["phrases"]["configure_success"].format(f"{comm} {value1}", value2))
+                        else:
+                            raise ValueError
+                    except sqlite3.DatabaseError:
+                        await ctx.send(lang(ctx)["phrases"]["error_db"])
+                    except ValueError:
+                        await ctx.send(lang(ctx)["phrases"]["configure_fail_int"])
+                case "time":
+                    try:
+                        value = int(value2)
+                        if value <= 120 and value >= 5:
+                            c.execute("UPDATE guilds SET mathtime = ? WHERE gid = ?", (value, ctx.guild.id,))
+                            base.commit()
+                            await ctx.send(lang(ctx)["phrases"]["configure_success"].format(f"{comm} {value1}", value2))
+                        else:
+                            raise ValueError
+                    except sqlite3.DatabaseError:
+                        await ctx.send(lang(ctx)["phrases"]["error_db"])
+                    except ValueError:
+                        await ctx.send(lang(ctx)["phrases"]["configure_fail_int"])
+                case "ops":
+                    if any(char not in "+-*/^" for char in value2):
+                        await ctx.send(lang(ctx)["phrases"]["configure_fail_other"])
+                        print("ended in cycle 1")
+                    else:
+                        if any(value2.count(char) > 1 for char in value2):
+                            await ctx.send(lang(ctx)["phrases"]["configure_fail_other"])
+                            print("ended in cycle 2")
+                            for char in value2:
+                                print(value2.count(char))
+                        else:
+                            try:
+                                c.execute("UPDATE guilds SET mathops = ? WHERE gid = ?", (value2, ctx.guild.id,))
+                                base.commit()
+                                await ctx.send(lang(ctx)["phrases"]["configure_success"].format(value1, value2))
+                            except sqlite3.DatabaseError:
+                                await ctx.send(lang(ctx)["phrases"]["error_db"])
+                case _:
+                    await ctx.send(lang(ctx)["phrases"]["configure_unknown"])
+        case "guess":
+            match value1:
+                case "range":
+                    try:
+                        value = int(value2)
+                        if value <= 1000 and value >= 5:
+                            c.execute("UPDATE guilds SET mathrange = ? WHERE gid = ?", (value, ctx.guild.id,))
+                            await ctx.send(lang(ctx)["phrases"]["configure_success"].format(f"{comm} {value1}", value2))
+                        else:
+                            raise ValueError
+                    except sqlite3.DatabaseError:
+                        await ctx.send(lang(ctx)["phrases"]["error_db"])
+                    except ValueError:
+                        await ctx.send(lang(ctx)["phrases"]["configure_fail_int"])
+                case "time":
+                    try:
+                        value = int(value2)
+                        if value <= 120 and value >= 5:
+                            c.execute("UPDATE guilds SET mathtime = ? WHERE gid = ?", (value, ctx.guild.id,))
+                            base.commit()
+                            await ctx.send(lang(ctx)["phrases"]["configure_success"].format(f"{comm} {value1}", value2))
+                        else:
+                            raise ValueError
+                    except sqlite3.DatabaseError:
+                        await ctx.send(lang(ctx)["phrases"]["error_db"])
+                    except ValueError:
+                        await ctx.send(lang(ctx)["phrases"]["configure_fail_int"])
+                case "tries":
+                    try:
+                        value = int(value2)
+                        if value <= 32 and value >= 1:
+                            c.execute("UPDATE guilds SET mathtime = ? WHERE gid = ?", (value, ctx.guild.id,))
+                            base.commit()
+                            await ctx.send(lang(ctx)["phrases"]["configure_success"].format(f"{comm} {value1}", value2))
+                        else:
+                            raise ValueError
+                    except sqlite3.DatabaseError:
+                        await ctx.send(lang(ctx)["phrases"]["error_db"])
+                    except ValueError:
+                        await ctx.send(lang(ctx)["phrases"]["configure_fail_int"])
+                case _:
+                    await ctx.send(lang(ctx)["phrases"]["configure_unknown"])
+        case "welcome":
+            if value1 == "null":
+                c.execute("UPDATE guilds SET welcome = NULL WHERE gid = ?", (ctx.guild.id,))
+                base.commit()
+                await ctx.send(lang(ctx)["phrases"]["configure_success"].format(comm, "none"))
+            elif value1:
+                formats = re.findall(r"(?<=(?<!\{)\{)[^{}]*(?=\}(?!\}))", value1)
+                try:
+                    for form in formats:
+                        if int(form) > 3:
+                            raise TypeError
+                        else:
+                            continue
+                except TypeError:
+                    await ctx.send(lang(ctx)["phrases"]["configure_fail_format"])
+                    return
+                except sqlite3.DatabaseError:
+                    await ctx.send(lang(ctx)["phrases"]["error_db"])
+                    return
+                c.execute("UPDATE guilds SET welcome = ? WHERE gid = ?", (value1, ctx.guild.id))
+                base.commit()
+                await ctx.send(lang(ctx)["phrases"]["configure_success"].format(comm, f'"{value1}"'))
+            else:
+                c.execute("SELECT welcome FROM guilds WHERE gid = ?", (ctx.guild.id,))
+                response = c.fetchone()
+                if response:
+                    await ctx.send(response[0])
+                else:
+                    await ctx.send(lang(ctx)["phrases"]["welcome_null"])
+                
+                    
+        case _:
+            await ctx.send(lang(ctx)["phrases"]["configure_unknown"])
+@commands.has_guild_permissions(kick_members=True)
+@commands.guild_only()
+@bot.command()
+async def kick(ctx, member : discord.Member, reason = None):
+    await member.kick(reason=reason)
+    if reason:
+        reason = f", {reason}"
     else:
-        await ctx.send(lang(ctx)["phrases"]["math_fail_wrong"].format(num))
+        reason = ""
+    await ctx.send(lang(ctx)["phrases"]["kick_reason"].format(member, ctx.author.mention, reason))
+@commands.has_guild_permissions(ban_members=True)
+@commands.guild_only()
+@bot.command()
+async def ban(ctx, member : discord.Member, reason = None):
+    await member.ban(reason=reason)
+    if reason:
+        reason = f", {reason}"
+    else:
+        reason = ""
+    await ctx.send(lang(ctx)["phrases"]["ban_reason"].format(member, ctx.author.mention, reason))
+@commands.has_guild_permissions(moderate_members=True)
+@commands.guild_only()
+@bot.command()
+async def mute(ctx, member : discord.Member, reason = None, days : int = 0, hours : int = 0, minutes : int = 5):
+    duration = timedelta(minutes=minutes, hours=hours, days=days)
+    await member.timeout(duration, reason=reason)
+    if reason:
+        reason = f", {reason}"
+    else:
+        reason = ""
+    await ctx.send(lang(ctx)["phrases"]["mute_reason"].format(member, duration, ctx.author.mention, reason))
+@commands.has_guild_permissions(moderate_members=True)
+@commands.guild_only()
+@bot.command()
+async def unmute(ctx, member : discord.Member):
+    await member.timeout(None)
+    await ctx.send(lang(ctx)["phrases"]["unmute_reason"].format(member.mention, ctx.author.mention)) 
+@bot.event
+async def on_command_error(ctx, error):
+    match type(error):
+        case commands.errors.MissingRequiredArgument:
+            await ctx.send(lang(ctx)["phrases"]["error_specification"])
+        case commands.errors.MissingPermissions:
+            await ctx.send(lang(ctx)["phrases"]["error_permissions_your"])
+        case discord.Forbidden:
+            await ctx.send(lang(ctx)["phrases"]["error_permissions_mine"])
+        case commands.errors.BadArgument:
+            await ctx.send(lang(ctx)["phrases"]["error_argument"])
+        case commands.errors.NoPrivateMessage:
+            await ctx.send(lang(ctx)["phrases"]["error_private"])
+        case commands.errors.CommandNotFound:
+            await ctx.send(lang(ctx)["phrases"]["configure_unknown"])
+        case commands.errors.CommandOnCooldown:
+            pass
+        case _:
+            await ctx.send(lang(ctx)["phrases"]["error"])
+            print(error)
 bot.run(token)
